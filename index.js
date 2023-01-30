c9e71b88b39ff969f116e6a39a9847659097c23a
johanvaneck <95653001+johanvaneck@users.noreply.github.com>
2023-01-30 09:09:46 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 // Import stylesheets
 import './style.css';
 
-function getClass(){
+let students = []
 
-}
 
-function markPresence(){
-  
-}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    const clickedButton = this.innerText;
+    switch (clickedButton) {
+      case 'Grade 0':
+      case 'Grade 1':
+      case 'Grade 2':
+      case 'Grade 3':
+      case 'Grade 4':
+      case 'Grade 5':
+      case 'Grade 6':
+      case 'Grade 7':
+        alert("Grade Button Clicked")
+        break;
+      case 'Previous':
+        alert("Previous Button Clicked")
+        break;
+      case 'Absent':
+        alert("Absent Button Clicked")
+        break;
+      case 'Present':
+        alert("Present Button Clicked")
+        break;
+    }
+  });
+});
